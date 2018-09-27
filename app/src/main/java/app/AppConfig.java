7f94bd2270b85dfa2e3e98c50d69d39be37f1eb0
Mostafa Tawfik <taty.vip1998@gmail.com>
2018-09-27 17:56:43 +0200

    Initial commit

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by Rama3 on 23/09/2018.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://172.20.10.5/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://172.20.10.5/android_login_api/register.php";
+}
